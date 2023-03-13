fec89e34e752ef7d39bbfd6b00cdb0c59581a79f
Jean Rondon <jeanrdev@gmail.com>
2023-03-13 00:24:16 -0400

    Configure Storybokk and using.

--- uiLib/src/Button/Button.jsx
+++ uiLib/src/Button/Button.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Button.scss';
+import './Button.css';
 
 const Button = ({ variant, ...props }) => {
   return (
---
+++ uiLib/src/Button/Button.stories.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Button from './Button';
+
+export default {
+  title: 'library/Button',
+  component: Button,
+};
+
+const Template = (args) => <Button {...args} />;
